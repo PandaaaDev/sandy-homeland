04c6a383e297b2dfae2f87bd9e09c9728c66cb19
Eryk Juszczak <juszczakeryk8@gmail.com>
2022-02-16 13:37:28 +0100

    I added new functionality to burgermenu. When you click on main page it will remove show class on menu

--- js/burgermenu,.js
+++ js/burgermenu,.js
@@ -1,7 +1,16 @@
 const burgermenu = document.querySelector('.nav-burgermenu__button')
 const nav = document.querySelector('.nav-menu')
-
+const main = document.querySelector('.main')
 const toggleMenu = () => {
 	nav.classList.toggle('show')
+	if (nav.classList[1] === 'show') {
+		burgermenu.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+	} else {
+		burgermenu.innerHTML = '<i class="fa-solid fa-bars-staggered"></i>'
+	}
+}
+const removeMenu = () => {
+	nav.classList.remove('show')
 }
 burgermenu.addEventListener('click', toggleMenu)
+main.addEventListener('click', removeMenu)
